f9da8596a0c310be64c9d680565b6c89e8f50c16
Ayan SARKAR <ayan.sarkar@ens.uvsq.fr>
2023-11-13 16:13:47 +0100

    fin tempsEnDate

--- td3.py
+++ td3.py
@@ -1,12 +1,8 @@
 # Fonction permettant de demander à l'utilisateur de rentrer un temps en jours, heures, minutes et secondes
 
 def demandeTemps():
-    années = int(input("Entrez le nombre d'années voulues : "))
-    mois = int(input("Entrez le nombre de mois voulus : "))
-    while mois > 12 or mois == ValueError:
-        mois = int(input("Entrez une valeur correcte : "))
     jours = int(input("Entrez le nombre de jours voulus : "))
-    while jours > 30 or jours == ValueError:
+    while jours == ValueError:
         jours = int(input("Entrez une valeur correcte : "))
     heures = int(input("Entrez le nombre d'heures voulues : "))
     while heures > 24 or heures == ValueError:
@@ -17,29 +13,25 @@ def demandeTemps():
     secondes = int(input("Entrez le nombre de secondes voulues : "))
     while secondes > 60 or secondes == ValueError:
         secondes = int(input("Entrez une valeur correcte : "))
-    return années, mois, jours, heures, minutes, secondes
+    return jours, heures, minutes, secondes
 
 # Fonction permettant de convertir en secondes le temps entré par l'utilisateur
 
-def tempsEnSecondes(temps):
-    années, mois, jours, heures, minutes, secondes = temps
-    temps_en_secondes = années * 365 * 24 * 3600 + mois * 30 * 24 * 3600 + jours * 24 * 3600 + heures * 3600 + minutes * 60 + secondes
+def tempsEnSecondes(temps): 
+    jours, heures, minutes, secondes = temps
+    temps_en_secondes = jours * 24 * 3600 + heures * 3600 + minutes * 60 + secondes
     return temps_en_secondes
 
 # Fonction permettant de convertir en jour, heures minutes et seconde un temps donné en secondes par l'utilisateur
 
 def secondeEnTemps(seconde):
-    années = seconde // (365 * 24 * 3600)
-    seconde = seconde % (365 * 24 * 3600)
-    mois = seconde // (30 * 24 * 3600)
-    seconde = seconde % (30 * 24 * 3600)
     jours = seconde // (24 * 3600)
     seconde = seconde % (24 * 3600)
     heures = seconde // 3600
     seconde = seconde % 3600
     minutes = seconde // 60
     seconde = seconde % 60
-    return années, mois, jours, heures, minutes, seconde
+    return jours, heures, minutes, seconde
 
 # Fonction permettant de déterminer si un mot doit être mis au pluriel ou non
 
@@ -49,12 +41,8 @@ def pluriel(n, mot):
 # Fonction permettant d'afficher un temps en jours, heures, minutes et secondes
 
 def afficheTemps(temps):
-    années, mois, jours, heures, minutes, secondes = temps
+    jours, heures, minutes, secondes = temps
     message = "Cela correspond à "
-    if années > 0:
-        message += f"{années} {pluriel(années, 'année')}, "
-    if mois > 0:
-        message += f"{mois} {pluriel(mois, 'mois')}, "
     if jours > 0:
         message += f"{jours} {pluriel(jours, 'jour')}, "
     if heures > 0:
@@ -62,7 +50,7 @@ def afficheTemps(temps):
     if minutes > 0:
         message += f"{minutes} {pluriel(minutes, 'minute')}, "
     if secondes > 0:
-        message += f"{secondes} {pluriel(secondes, 'seconde')}, "
+        message += f"{secondes} {pluriel(secondes, 'seconde')}"
     print(message, end="\n")
 
 # Fonction permettant d'additionner deux temps distincts en les convertissant en secondes, puis en affichant le total normalement
@@ -75,34 +63,36 @@ def sommeTemps(temps1, temps2):
 # Fonction permettant de calculer le pourcentage d'un temps
 
 def proportionTemps(temps, pourcentage):
-    print("Quel est le temps dont vous souhaitez connaître la proportion ?")
-    temps = demandeTemps()
-    temps_sec = tempsEnSecondes(temps)
-    pourcentage = float(input("Quel est le pourcentage de ce temps que vous souhaitez connaître ? ")) / 100
-    temps_pourcentage = secondeEnTemps(temps_sec * pourcentage)
+    temps_seconde = tempsEnSecondes(temps)
+    temps_pourcentage = secondeEnTemps(temps_seconde * pourcentage)
     resultat_final = afficheTemps(temps_pourcentage)
     return resultat_final
 
 # Fonction prenant un temps en entrée et renvoyant une date en années, mois, jours, minutes, secondes (à partir du 1er janvier 1970) 
 
 def tempsEnDate(temps_utilisateur):
-    temps_départ = (1970, 0, 0, 0, 0, 0)
+    temps_départ = (1970*365, 0, 0, 0)
     temps_final = sommeTemps(temps_départ, temps_utilisateur)
     return temps_final
 
 # Fonction permettant d'afficher une date au format
 
-def afficheDate():
+def afficheDate(temps_utilisateur):
     temps_date = tempsEnDate(temps_utilisateur)
-    années, mois, jours, heures, minutes, secondes = temps_date
-    message = f"La date finale est la suivante : "
-    if jours > 0:
-        message += f"{jours} "
+    jours, heures, minutes, secondes = temps_date
+    années, mois = 0, 0
+    années = jours // 365
+    jours_restants = jours % 365
+    mois = jours_restants // 30  
+    jours_restants = mois % 30
+    message = "La date finale est la suivante : "
+    mois_nom = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"]
+    if jours_restants > 0:
+        message += f"{jours_restants} "
     else:
         message += "1er "
-    mois_nom = ["janvier ", "février ", "mars ", "avril ", "mai ", "juin ", "juillet ", "août ", "septembre ", "octobre ", "novembre ", "décembre "]
-    message += f"{mois_nom[mois]}"
-    message += f"{années}"
+    message += f"{mois_nom[mois]} "
+    message += f"{années}\n"
     print(message)
 
 #  Fonction permettant d'afficher toutes les années bissextiles depuis 1970
@@ -110,7 +100,7 @@ def afficheDate():
 def bissextile(jours):
     année = jours // 365 + 1970
     for i in range(1970, année):
-        if i%4 == 0:
+        if (i % 4 == 0 and i % 100 != 0) or (i % 400 == 0):
             print(i)
 
 # Demande à l'utilisateur de choisir une option
@@ -131,8 +121,8 @@ somme_temps = input("Voulez-vous effectuer la somme de deux temps ? Oui [Y], non
 if somme_temps.lower() == "y":
     temps1 = demandeTemps()
     temps2 = demandeTemps()
-    résultat = sommeTemps(temps1, temps2)
-    print(afficheTemps(résultat))
+    resultat = sommeTemps(temps1, temps2)
+    print(afficheTemps(resultat))
 
 prop_temps = input("Voulez-vous obtenir la proportion d'un temps ? Oui [Y], non [N] ")
 if prop_temps.lower() == "y":
@@ -143,7 +133,7 @@ if prop_temps.lower() == "y":
 temps_en_date = input("Voulez-vous obtenir un temps sous forme de date (depuis le 1er janvier 1970 ? Oui [Y], non [N] ")
 if temps_en_date.lower() == "y":
     temps_utilisateur = demandeTemps()
-    afficheDate()
+    afficheDate(temps_utilisateur)
 
 années_bissextiles = input("Voulez-vous afficher toutes les années bissextiles depuis 1970 jusqu'à une date ? Oui [Y], non [N] ")
 if années_bissextiles.lower() == "y":
