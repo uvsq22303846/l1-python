54d6fb98ec6284d4cb6092ca6c0f12860ff05901
Ayan SARKAR <ayan.sarkar@ens.uvsq.fr>
2024-02-09 13:49:40 +0100

    fin td

--- S2/test.py
+++ S2/test.py
@@ -1,3 +1,36 @@
-import numpy as np 
+import tkinter as tk
+import PIL as pil
+from PIL import Image
+from PIL import ImageTk
+from tkinter import filedialog
+from tkinter import simpledialog
 
-print(np.cosh(1))
+create = True
+
+def charger(widg):
+    global create
+    global photo
+    global img
+    global canvas
+    filename = filedialog.askopenfile(mode= "rb", title = "Choose a file")
+    img = Image.open(filename)
+    photo = ImageTk.PhotoImage(img)
+    print(photo)
+    if create:
+        canvas = tk.Canvas(widg, width = img.size[0], height = img.size[1])
+        canvas.create_image(0, 0, anchor = tk.NW, image = photo)
+        canvas.pack()
+        create = False
+    else:
+        canvas.pack_forget()
+        canvas = tk.Canvas(widg, width = img.size[0], height = img.size[1])
+        canvas.create_image(0, 0, anchor = tk.NW, image = photo)
+        canvas.pack()
+
+root = tk.Tk()
+root.title("J'apprends Tkinter")
+bouton = tk.Canvas(root, text = "Charger", command = lambda: charger(canvas)).pack()
+canvas = tk.Canvas(root, width = 300, height = 300).pack()
+
+
+root.mainloop()
